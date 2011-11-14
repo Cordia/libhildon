bdb31d01651aeae6fb3d3a46a8ed091b0b774206
Tomasz Sterna <tomek@xiaoka.com>
2011-11-14 22:47:41 +0100

    Merged imcontext.patch

--- hildon/hildon-text-view.c
+++ hildon/hildon-text-view.c
@@ -164,8 +164,12 @@ hildon_text_view_button_press_event             (GtkWidget        *widget,
 
     gtk_widget_grab_focus (widget);
 
-    if (GTK_TEXT_VIEW (widget)->editable &&
-        hildon_gtk_im_context_filter_event (GTK_TEXT_VIEW (widget)->im_context, (GdkEvent*)event)) {
+    if (GTK_TEXT_VIEW (widget)->editable 
+#ifdef MAEMO_GTK
+        && hildon_gtk_im_context_filter_event (GTK_TEXT_VIEW (widget)->im_context, (GdkEvent*)event)
+#endif
+        )
+    {
         GTK_TEXT_VIEW (widget)->need_im_reset = TRUE;
         return TRUE;
     }
@@ -214,8 +218,12 @@ hildon_text_view_button_release_event           (GtkWidget        *widget,
     GtkTextIter iter;
     gint x, y;
 
-    if (text_view->editable &&
-        hildon_gtk_im_context_filter_event (text_view->im_context, (GdkEvent*)event)) {
+    if (text_view->editable
+#ifdef MAEMO_GTK
+        && hildon_gtk_im_context_filter_event (text_view->im_context, (GdkEvent*)event)
+#endif
+        )
+    {
         text_view->need_im_reset = TRUE;
         return TRUE;
     }
